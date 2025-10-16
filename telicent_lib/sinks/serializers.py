6e8fc0d98b38427486e5d52f8dc40c486a29a95d
Daniel Davies <110399083+danieldaviestelicent@users.noreply.github.com>
2025-10-16 09:42:34 +0100

    chore!: removed RDF serializer and deserializer

--- telicent_lib/sinks/serializers.py
+++ telicent_lib/sinks/serializers.py
@@ -1,13 +1,8 @@
 from __future__ import annotations
 
 import json
-import warnings
 import zlib
 from typing import Any, Protocol, runtime_checkable
-
-# noinspection PyProtectedMember
-from confluent_kafka.serialization import Serializer
-from rdflib import Graph
 
 __license__ = """
 Copyright (c) Telicent Ltd.
@@ -123,54 +118,3 @@ class Serializers:
         if data is None:
             return None
         return Serializers.to_binary(json.dumps(data))
-
-
-class RdfSerializer(Serializer):
-    """
-    A kafka-python serializer for RDF Graphs and Datasets
-    """
-
-    def __init__(self, rdf_format: str = "nquads", require_context_awareness: bool = True,
-                 rdf_graph_format: str = "nt11"):
-        """
-        Creates a new serializer
-        :param rdf_format:
-            The RDF format to use for serialization.  If not specified we're using NQuads as the default.
-        :param require_context_awareness:
-            Whether the given `rdf_format` requires a context aware value i.e. a Dataset rather than a Graph, if True
-            and the value to serialize is not context aware then falls back to the `rdf_graph_format` instead.
-        :param rdf_graph_format:
-            Alternative RDF format to use for serialization if the provided value to serialize is not context aware i.e.
-            a Dataset.  Defaults to NTriples 1.1 since that's a subset of the NQuads format used as the default for
-            `rdf_format`
-        """
-        warnings.warn(
-            "`telicent_lib.sinks.serializers.RdfSerializer` has been deprecated. "
-            "You should serialize RDF data yourself before calling `send()`.",
-            DeprecationWarning,
-            stacklevel=2
-        )
-        super().__init__()
-        self.rdf_format = rdf_format
-        self.require_context_awareness = require_context_awareness
-        self.rdf_graph_format = rdf_graph_format
-
-    def __call__(self, obj, ctx=None):
-        """
-        Serializes the value assuming it's a rdflib Graph (or subclass thereof, e.g., Dataset) into a byte sequence
-
-        :param value: Value to serialize
-        :return: Serialized RDF
-        """
-        if obj is None:
-            return None
-
-        if not isinstance(obj, Graph):
-            raise TypeError(f"Cannot serialize a value of type {type(obj)}, expected a rdflib Graph/Dataset")
-
-        # Intentionally having RDFLib produce us bytes directly, this avoids an extra unnecessary bytes -> str -> bytes
-        # round tripping.
-        if not obj.context_aware and self.require_context_awareness:
-            return obj.serialize(format=self.rdf_graph_format, encoding="utf-8")
-        else:
-            return obj.serialize(format=self.rdf_format, encoding="utf-8")
