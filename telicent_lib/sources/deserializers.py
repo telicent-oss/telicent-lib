7791c69b0ef52caeb831f32f6697ac4a8185ac83
Daniel Davies <110399083+danieldaviestelicent@users.noreply.github.com>
2025-10-16 11:29:03 +0100

    chore!: removed RDF serializer and deserializer (#77)
    * code changes and fixed tests
    
    * chore!: removed RDF serializer and deserializer
    
    * mypy fixes
    
    * rollback version bumps
    
    * remove old tests
    
    * rollback colour fixes
    
    * rollback colour fixes

--- telicent_lib/sources/deserializers.py
+++ telicent_lib/sources/deserializers.py
@@ -1,12 +1,6 @@
 import json
-import warnings
 import zlib
 from typing import Any, Protocol, runtime_checkable
-
-# noinspection PyProtectedMember
-from confluent_kafka.serialization import Deserializer
-from rdflib import Dataset
-from rdflib.graph import DATASET_DEFAULT_GRAPH_ID
 
 __license__ = """
 Copyright (c) Telicent Ltd.
@@ -85,60 +79,3 @@ class Deserializers:
         if data is None:
             return None
         return json.loads(data.decode('utf-8'))
-
-
-class RdfDeserializer(Deserializer):
-    """
-    A kafka-python deserializer for RDF datasets
-    """
-
-    def __init__(self, rdf_format="nquads"):
-        """
-        Creates a new deserializer
-
-        :param rdf_format:
-            The default RDF format to assume when deserializing.  If not specified we're using NQuads as the default
-        """
-        warnings.warn(
-            "`telicent_lib.sources.deserializer.RdfDeserializer` has been deprecated. "
-            "You should deserialize RDF data yourself.",
-            DeprecationWarning,
-            stacklevel=2
-        )
-        self.rdf_format = rdf_format
-
-    def __call__(self, value, ctx=None):
-        """
-        Deserializes a byte sequence into an RDF Dataset
-
-        :param topic: Topic
-        :param bytes_: Byte sequence to deserialize
-        :return: RDF Dataset
-        """
-        if value is None:
-            return None
-
-        # Default to our configured default format
-        actual_format = self.rdf_format
-
-        # See Issue #23 - kafka-python doesn't currently provide access to headers at deserialization time
-        # This needs us to get upstream improvements accepted there before we can support the following logic
-        # Currently this isn't a blocker because all deployments will have events flowing through the system in the same
-        # RDF serialization but longer term as we have deployed customer deployments and migrate to Binary RDF/RDF
-        # Deltas so topics contain messages in multiple formats this will become an issue
-        # if headers is not None:
-        #    for key, value in headers:
-        #        if key == "Content-Type":
-        #            actual_format = value.decode("utf-8")
-        #            break
-
-        # Parses into a dataset, so that we can support separate data and label graphs
-
-        # Due to RDFLib Issue #1804 we have to explicitly set the publicID to be the default Graph ID as otherwise
-        # when we parse in data that doesn't have an explicit graph set, i.e. is intended for the default graph, then
-        # the default RDFLib behaviour puts that into a blank node named graph.  Specifying their internal Default Graph
-        # ID explicitly ensures the triples go to the default graph as intended.
-        # https://github.com/RDFLib/rdflib/issues/1804
-        g = Dataset()
-        g.parse(data=value, format=actual_format, publicID=DATASET_DEFAULT_GRAPH_ID)
-        return g
--- tests/test_serdes.py
+++ tests/test_serdes.py
@@ -6,14 +6,11 @@ from typing import Any
 
 from confluent_kafka import KafkaException
 from confluent_kafka.serialization import Deserializer, Serializer
-from rdflib import RDF, RDFS, BNode, Dataset, Graph, URIRef
-from rdflib.compare import to_isomorphic
-from rdflib.graph import DATASET_DEFAULT_GRAPH_ID
 
 from telicent_lib.sinks import KafkaSink, Serializers
-from telicent_lib.sinks.serializers import RdfSerializer, SerializerFunction
+from telicent_lib.sinks.serializers import SerializerFunction
 from telicent_lib.sources import Deserializers, KafkaSource
-from telicent_lib.sources.deserializers import DeserializerFunction, RdfDeserializer
+from telicent_lib.sources.deserializers import DeserializerFunction
 
 
 def __default_serdes__() -> list[tuple[Callable[[Any], bytes | None], Callable[[bytes | None], bytes | Any]]]:
@@ -22,44 +19,6 @@ def __default_serdes__() -> list[tuple[Callable[[Any], bytes | None], Callable[[
         (Serializers.to_zipped_binary, Deserializers.unzip_to_string),
         (Serializers.to_json, Deserializers.from_json)
     ]
-
-
-def __compare_rdf_graphs__(a: Any, b: Any) -> bool:
-    if isinstance(a, Dataset) and isinstance(b, Dataset):
-        # If we're comparing two datasets compare each graph within them
-        for a_ctx in a.contexts():
-            b_ctx = b.get_context(identifier=a_ctx.identifier)
-            if not __compare_rdf_graphs__(a_ctx, b_ctx):
-                return False
-        for b_ctx in b.contexts():
-            a_ctx = a.get_context(identifier=b_ctx.identifier)
-            if not __compare_rdf_graphs__(a_ctx, b_ctx):
-                return False
-        return True
-
-    if not isinstance(a, Dataset):
-        a_iso = to_isomorphic(a)
-    else:
-        a_iso = to_isomorphic(a.get_context(identifier=DATASET_DEFAULT_GRAPH_ID))  # next(a.contexts()))
-    if not isinstance(b, Dataset):
-        b_iso = to_isomorphic(b)
-    else:
-        b_iso = to_isomorphic(b.get_context(identifier=DATASET_DEFAULT_GRAPH_ID))  # next(b.contexts()))
-
-    # For debugging if tests aren't producing the expected results
-    if False:
-        print("A:")
-        print(a_iso.serialize(destination=None, encoding=None, format="nt11"))
-        print()
-        print("B:")
-        if isinstance(b, Dataset):
-            print("Full Dataset:")
-            print(b.serialize(destination=None, encoding=None, format="nquads"))
-            for _, ctx in enumerate(b.contexts()):
-                print(ctx.identifier)
-        print(b_iso.serialize(destination=None, encoding=None, format="nt11"))
-
-    return a_iso == b_iso
 
 
 class ExtendedSerializerFunction(SerializerFunction):
@@ -160,95 +119,6 @@ class TestSerdes(unittest.TestCase):
         with self.assertRaises(JSONDecodeError):
             Deserializers.from_json(Serializers.to_binary(bad_json))
 
-    def test_graph_none(self):
-        serializer = RdfSerializer()
-        self.assertIsNone(serializer(None))
-        deserializer = RdfDeserializer()
-        self.assertIsNone(deserializer(None))
-
-    def test_graph_empty(self):
-        g = Graph()
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_graph_simple(self):
-        g = Graph()
-        g.add((BNode(), RDF.type, RDFS.Class))
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_graph_alternate_format(self):
-        g = Graph()
-        g.add((BNode(), RDF.type, RDFS.Class))
-        self._verify_round_trip(g, serializer_class=RdfSerializer("trig", True, "ttl"),
-                                deserializer_class=RdfDeserializer("trig"),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_dataset_empty(self):
-        g = Dataset()
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_dataset_simple(self):
-        g = Dataset()
-        g.add((BNode(), RDF.type, RDFS.Class))
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_dataset_multiple_graphs_01(self):
-        g = Dataset()
-        g.add((BNode(), RDF.type, RDFS.Class))
-        g.add((URIRef("https://example.org/test"), RDF.type, RDFS.Class, URIRef("https://graph")))
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_dataset_multiple_graphs_02(self):
-        g = Dataset()
-        g.add((URIRef("https://example.org/first"), RDF.type, RDFS.Class))
-        g.add((URIRef("https://example.org/second"), RDF.type, RDFS.Class, URIRef("https://graph")))
-        g.add((URIRef("https://example.org/third"), RDF.type, RDFS.Class, DATASET_DEFAULT_GRAPH_ID))
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_dataset_multiple_graphs_03(self):
-        g = Dataset()
-        g.add((BNode(), RDF.type, RDFS.Class))
-        g.add((BNode(), RDF.type, RDFS.Class, URIRef("https://graph")))
-        g.add((BNode(), RDF.type, RDFS.Class, DATASET_DEFAULT_GRAPH_ID))
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_dataset_multiple_graphs_04(self):
-        g = Dataset()
-        subject = BNode()
-        g.add((subject, RDF.type, RDFS.Class))
-        g.add((subject, RDF.type, RDFS.Class, URIRef("https://graph")))
-        g.add((subject, RDF.type, RDFS.Class, DATASET_DEFAULT_GRAPH_ID))
-        self._verify_round_trip(g, serializer_class=RdfSerializer(), deserializer_class=RdfDeserializer(),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_dataset_multiple_graphs_alternate_format(self):
-        g = Dataset()
-        g.add((URIRef("https://example.org/first"), RDF.type, RDFS.Class))
-        g.add((URIRef("https://example.org/second"), RDF.type, RDFS.Class, URIRef("https://graph")))
-        g.add((URIRef("https://example.org/third"), RDF.type, RDFS.Class, DATASET_DEFAULT_GRAPH_ID))
-        self._verify_round_trip(g, serializer_class=RdfSerializer("trig", True, "ttl"),
-                                deserializer_class=RdfDeserializer("trig"),
-                                comparison_function=__compare_rdf_graphs__)
-
-    def test_serdes_validation_01(self):
-        for serdes in __default_serdes__():
-            serializer = serdes[0]
-            deserializer = serdes[1]
-            with self.subTest(serializer=serializer, deserializer=deserializer):
-                self.assertTrue(inspect.ismethod(serializer) or inspect.isfunction(serializer))
-                self.assertTrue(inspect.ismethod(deserializer) or inspect.isfunction(deserializer))
-
-        self.assertTrue(isinstance(RdfSerializer(), Serializer))
-        self.assertTrue(isinstance(RdfDeserializer(), Deserializer))
-        self.assertFalse(inspect.ismethod(RdfSerializer) and inspect.isfunction(RdfSerializer))
-        self.assertFalse(inspect.ismethod(RdfDeserializer) and inspect.isfunction(RdfDeserializer))
-
     def test_serdes_validation_02(self):
         self.assertFalse(inspect.ismethod(ExtendedSerializerFunction) or inspect.isfunction(ExtendedSerializerFunction))
         self.assertTrue(isinstance(ExtendedSerializerFunction, SerializerFunction))
@@ -278,54 +148,6 @@ class TestSerdes(unittest.TestCase):
                         kafka_config={'bootstrap.servers': 'localhost:12345'},
                         key_deserializer=ExtendedDeserializerFunction,
                         value_deserializer=ExtendedDeserializerFunction)
-
-    def test_serdes_validation_05(self):
-        self.assertFalse(inspect.ismethod(ExtendedSerializerFunction) or inspect.isfunction(ExtendedSerializerFunction))
-        self.assertTrue(isinstance(ExtendedSerializerFunction, SerializerFunction))
-
-        with self.assertRaises(expected_exception=KafkaException):
-            KafkaSource(topic="test",
-                        kafka_config={'bootstrap.servers': 'localhost:12345'},
-                        key_deserializer=RdfDeserializer(),
-                        value_deserializer=RdfDeserializer())
-
-    def test_serdes_validation_bad_01(self):
-
-        # KafkaSink(topic="test", key_serializer=RdfSerializer)
-        with self.assertRaisesRegex(expected_exception=TypeError,
-                                    expected_regex="not a function/method/Serializer instance.*instance of this class"):
-            KafkaSink(
-                topic="test",
-                key_serializer=RdfSerializer
-            )
-
-    def test_serdes_validation_bad_02(self):
-        with self.assertRaisesRegex(expected_exception=TypeError,
-                                    expected_regex="not a function/method/Serializer instance.*instance of this class"):
-            KafkaSink(
-                topic="test",
-                value_serializer=RdfSerializer
-            )
-
-    def test_serdes_validation_bad_03(self):
-        with self.assertRaisesRegex(
-                expected_exception=TypeError,
-                expected_regex="not a function/method/Deserializer instance.*instance of this class"
-        ):
-            KafkaSource(
-                topic="test",
-                key_deserializer=RdfDeserializer
-            )
-
-    def test_serdes_validation_bad_04(self):
-        with self.assertRaisesRegex(
-                expected_exception=TypeError,
-                expected_regex="not a function/method/Deserializer instance.*instance of this class"
-        ):
-            KafkaSource(
-                topic="test",
-                value_deserializer=RdfDeserializer
-            )
 
 
 if __name__ == '__main__':
